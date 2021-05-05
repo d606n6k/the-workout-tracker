d08760a7f45cf3862d5d3c61a2093a7d77ebd188
William Lucht <aaronlucht@gmail.com>
2021-05-05 15:08:07 -0700

    added mongodb connection to server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutsdb", {
+  useNewUrlParser: true,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
