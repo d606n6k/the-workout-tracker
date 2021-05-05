5380d1f6d47276ad343f87ba124a1159642fd8d3
William Lucht <aaronlucht@gmail.com>
2021-05-05 15:04:07 -0700

    initial server.js lines of code to serve and run server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-// need to hook up server code with mongodb
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
