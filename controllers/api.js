ef8bb33ddb2eed6a52553f9606d723d79932fa00
William Lucht <aaronlucht@gmail.com>
2021-05-06 17:30:39 -0700

    reconfigured Workout model and updated api route

--- controllers/api.js
+++ controllers/api.js
@@ -16,9 +16,7 @@ router.get("/workouts", async (req, res) => {
 router.get("/workouts/range", async (req, res) => {
   try {
     // const lastWorkout = await db.Workout.find({});
-    const lastWorkout = await await db.Workout.find({})
-      .limit(7)
-      .sort({ _id: -1 });
+    const lastWorkout = await db.Workout.find({}).limit(7).sort({ _id: -1 });
     const revereTheWorkOut = lastWorkout.reverse();
     res.status(200).json(revereTheWorkOut);
   } catch (error) {
@@ -28,8 +26,26 @@ router.get("/workouts/range", async (req, res) => {
 
 // PUT route - add an exercise to the db
 // fetch("/api/workouts/" + id)
+router.put("/workouts/:id", async (req, res) => {
+  try {
+    const newWorkout = await db.Workout.findByIdAndUpdate(req.params.id, {
+      $push: { exercises: req.body },
+    });
+    res.status(200).json(newWorkout);
+  } catch (error) {
+    res.status(500);
+  }
+});
 
 // POST route - create a new workout and add to db
 // fetch("/api/workouts");
+router.post("/workouts", async (req, res) => {
+  try {
+    const newWorkout = await db.Workout.create(req.body);
+    res.status(200).json(newWorkout);
+  } catch (error) {
+    res.status(500);
+  }
+});
 
 module.exports = router;
--- models/Workout.js
+++ models/Workout.js
@@ -6,33 +6,38 @@ const workoutSchema = new Schema({
   // name, type, weight, sets, reps, and duration of exercise
   day: {
     type: Date,
+    default: Date.now,
   },
-  exercises: {
-    name: {
-      type: String,
-      trim: true,
-      required: "Enter a name for workout",
+  exercises: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for workout",
+      },
+      type: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        require: true,
+      },
+      distance: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
     },
-    type: {
-      type: String,
-      trim: true,
-    },
-    duration: {
-      type: Number,
-    },
-    weight: {
-      type: Number,
-      required: "Enter a weight",
-    },
-    reps: {
-      type: Number,
-      required: "How many reps did you do?",
-    },
-    sets: {
-      type: Number,
-      required: "Enter how many sets completed",
-    },
-  },
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
