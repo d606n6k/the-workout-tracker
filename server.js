91338c68720f71673cb5d751cb85dde604e82cff
William Lucht <aaronlucht@gmail.com>
2021-05-07 15:04:20 -0700

    deploying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const appRouter = require("./controllers");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
